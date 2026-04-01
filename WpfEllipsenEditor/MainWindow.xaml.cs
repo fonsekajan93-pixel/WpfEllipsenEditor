e2f3af7a5e34b4c9484f9ee61289b17376a11417
Jan Fonseka <fonsekaj2@bswiesaude.onmicrosoft.com>
2026-04-01 04:30:37 +0200

    Ellipse mit zufälliger Größe, Farbe und Position erzeugt

--- WpfEllipsenEditor/MainWindow.xaml.cs
+++ WpfEllipsenEditor/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ namespace WpfEllipsenEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Random Klasse erstellt
+        Random r = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,19 +29,32 @@ namespace WpfEllipsenEditor
 
         private void btnCreateEllipse_Click(object sender, RoutedEventArgs e)
         {
-            // Ellipse became create
+            // 1. Ellipse erstellen
             Ellipse ellipse = new Ellipse();
 
-            // set height and width of ellipse
-            ellipse.Width = 60;
-            ellipse.Height = 50;
+            // 2. Zufällige Größe 
+            ellipse.Width = r.NextInt64(20, 180);
+            ellipse.Height = r.NextInt64(20, 180);
 
-            // set colour of ellipse
-            ellipse.Fill = Brushes.Red;
+            // 3. Zufällige Farbe
+            byte rValue = (byte)r.Next(0, 256);
+            byte gValue = (byte)r.Next(0, 256);
+            byte bValue = (byte)r.Next(0, 256);
 
-            Canvas.SetLeft(ellipse, 20);
-            Canvas.SetTop(ellipse, 20);
+            Color c = Color.FromRgb(rValue, gValue, bValue);
+            ellipse.Fill = new SolidColorBrush(c);
 
+            // 4. Zufällige Position (mit Randprüfung)
+            int maxX = (int)(cnvsMain.ActualWidth - ellipse.Width);
+            int maxY = (int)(cnvsMain.ActualHeight - ellipse.Height);
+
+            int posX = r.Next(0, maxX);
+            int posY = r.Next(0, maxY);
+
+            Canvas.SetLeft(ellipse, posX);
+            Canvas.SetTop(ellipse, posY);
+
+            // 5. Ellipse wird hinzugefügt
             cnvsMain.Children.Add(ellipse);
         }
     }
