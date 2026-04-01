96958a7d6fbbf59db181d10b67d1117aa2543ef5
Jan Fonseka <fonsekaj2@bswiesaude.onmicrosoft.com>
2026-04-01 03:25:26 +0200

    Ellipse wird per Button zur Laufzeit erzeugt

--- WpfEllipsenEditor/MainWindow.xaml.cs
+++ WpfEllipsenEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,26 @@ namespace WpfEllipsenEditor
         public MainWindow()
         {
             InitializeComponent();
+            
+
+        }
+
+        private void btnCreateEllipse_Click(object sender, RoutedEventArgs e)
+        {
+            // Ellipse became create
+            Ellipse ellipse = new Ellipse();
+
+            // set height and width of ellipse
+            ellipse.Width = 60;
+            ellipse.Height = 50;
+
+            // set colour of ellipse
+            ellipse.Fill = Brushes.Red;
+
+            Canvas.SetLeft(ellipse, 20);
+            Canvas.SetTop(ellipse, 20);
+
+            cnvsMain.Children.Add(ellipse);
         }
     }
 }
